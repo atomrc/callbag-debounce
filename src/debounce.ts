f8902ef3c1b9476732b6d6398924f8118c6543e8
Thomas Belin <me@thomasbelin.fr>
2018-03-01 00:16:09 +0100

    Annotate the code

--- src/debounce.ts
+++ src/debounce.ts
@@ -1,14 +1,28 @@
-export function debounce(timeout: number) {
+/**
+ * Debounces the given listenable source
+ *
+ * @param {number} wait - The number of ms to wait before letting a value pass
+ * @returns {Function}
+ */
+export function debounce(wait: number): any {
   return (source: any) => (start: number, sink: any) => {
     if (start !== 0) return;
-    let timeoutId: number;
+    let timeout: number;
     source(0, (t: number, d: any) => {
       if (t === 1) {
-        if (timeoutId) {
-          clearTimeout(timeoutId);
+        // t === 1 means the source is emitting a value
+        if (timeout) {
+          clearTimeout(timeout);
         }
-        timeoutId = setTimeout(() => sink(1, d), timeout);
+        timeout = setTimeout(() => sink(1, d), wait);
       }
+      /*
+       * no need to handle the t === 0 case because
+       * the operator never needs to talkback to the source
+       *
+       * nothing specific to do when the source
+       * sends a t === 2 signal
+       */
       else sink(t, d);
     });
   };
