86d67989091df2e30cab9ae78e3c7fc96b010408
Thomas Belin <thomasbelin4@gmail.com>
2021-06-21 20:53:09 +0200

    chore: upgrade to latest callbag-mock and simplify tests (#15)

--- test.ts
+++ test.ts
@@ -1,133 +1,101 @@
 import * as test from "tape";
-
-const pipe = require("callbag-pipe");
-const forEach = require("callbag-for-each");
 const mock = require("callbag-mock");
-const subscribe = require("callbag-subscribe");
 
 import { debounce } from "./src/debounce";
 
 test("it should debounces a listenable source", (t) => {
   const timeoutValues = [0, 1, 2, 6, 16];
   const expected = [2, 6, 16];
-  t.plan(expected.length);
+  t.plan(1);
 
-  const source = mock("sources", true);
+  const source = mock(true);
+  const sink = mock();
 
-  pipe(
-    source,
-    debounce(2),
-    forEach((value) => t.equals(value, expected.shift()))
+  debounce(2)(source)(0, sink);
+
+  const timeouts = Promise.all(
+    timeoutValues.map((timeout) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          source.emit(1, timeout);
+          resolve(undefined);
+        }, timeout);
+      });
+    })
   );
 
-  timeoutValues.forEach((timeout) => {
-    setTimeout(() => source.emit(1, timeout), timeout);
+  timeouts.then(() => {
+    setTimeout(() => {
+      t.deepEqual(sink.getReceivedData(), expected);
+    }, 3);
   });
 });
 
 test("it should send error right away", (t) => {
   t.plan(2);
 
-  const source = mock("sources", function () {}, true);
-
-  const timeStart = new Date().getTime();
-  const debounceValue = 10000;
-  pipe(
-    source,
-    debounce(debounceValue),
-    (s) => (start, sink) => {
-      if (start !== 0) return;
-      s(0, (st, d) => {
-        if (st === 2) {
-          const exeTime = new Date().getTime() - timeStart;
-          t.ok(exeTime < debounceValue);
-          return t.equals(d, "error");
-        }
-        sink(st, d);
-      });
-    },
-    forEach(() => {})
-  );
+  const source = mock(true);
+  const sink = mock();
+  debounce(2)(source)(0, sink);
 
   source.emit(2, "error");
+  // Sink should have received end signal
+  t.equal(sink.checkConnection(), false);
+
+  // Sink should have received the error message
+  t.deepEqual(
+    (sink.getMessages() as any[]).find(([t]) => t === 2),
+    [2, "error"]
+  );
 });
 
-test("it should send completion right away if no value debounced", (t) => {
+test("it should send completion right away if no value is sent", (t) => {
   t.plan(1);
 
-  const source = mock("sources", function () {}, true);
+  const source = mock(true);
+  const sink = mock();
 
-  let timeStart: number;
-  const debounceValue = 10;
-  pipe(
-    source,
-    debounce(debounceValue),
-    (s) => (start, sink) => {
-      if (start !== 0) return;
-      s(0, (st, d) => {
-        if (st === 2) {
-          const exeTime = new Date().getTime() - timeStart;
-          return t.ok(exeTime < debounceValue);
-        }
-        sink(st, d);
-      });
-    },
-    forEach(() => {})
+  debounce(2)(source)(0, sink);
+
+  source.emit(2);
+  t.deepEqual(
+    sink.getMessages().find(([t]) => t === 2),
+    [2, undefined]
   );
+});
+
+test("it should send completion after the last emission", (t) => {
+  t.plan(2);
+  const source = mock(true);
+  const sink = mock();
+
+  debounce(2)(source)(0, sink);
 
   // emit a value
   source.emit(1, "data");
   // emit the completion once the value has been debounced
   setTimeout(() => {
-    timeStart = new Date().getTime();
     source.emit(2);
-  }, debounceValue + 1);
-});
-
-test("it should send completion after the last emission", (t) => {
-  t.plan(1);
-
-  const source = mock("sources", function () {}, true);
-
-  const timeStart = new Date().getTime();
-  const debounceValue = 50;
-  pipe(
-    source,
-    debounce(debounceValue),
-    (s) => (start, sink) => {
-      if (start !== 0) return;
-      s(0, (st, d) => {
-        if (st === 2) {
-          const exeTime = new Date().getTime() - timeStart;
-          return t.ok(exeTime >= debounceValue);
-        }
-        sink(st, d);
-      });
-    },
-    forEach(() => {})
-  );
-
-  source.emit(1, "event");
-  source.emit(2);
+    t.deepEqual(sink.getReceivedData(), ["data"]);
+    t.deepEqual(
+      sink.getMessages().find(([t]) => t === 2),
+      [2, undefined]
+    );
+  }, 3);
 });
 
 test("it should not emit after unsubscribe", (t) => {
   t.plan(1);
 
-  const source = mock("source", () => {}, true);
-  let res = false;
-  const unsubscribe = pipe(
-    source,
-    debounce(2),
-    subscribe((v) => {
-      res = v;
-    })
-  );
+  const source = mock(true);
+  const sink = mock();
+
+  debounce(2)(source)(0, sink);
 
   source.emit(1, true);
-  unsubscribe();
+  sink.emit(2);
 
   setTimeout(() => {
-    t.equal(res, false);
+    t.equal(sink.getReceivedData().length, 0);
   }, 5);
 });
